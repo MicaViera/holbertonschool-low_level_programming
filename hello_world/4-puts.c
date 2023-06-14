540d3c987d7d1077478616bbe360044e94ec2e33
MicaViera <vieranoelia98@gmail.com>
2023-06-14 13:14:55 -0700

    Puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -v puts "Programming is like building a multilingual puzzle"
+#include <stdio.h>
+
+/**
+ * main - puts function
+ *Return:0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return 0
+}
