cdab1690565113f9639c8a1684131a5f628e2e58
MicaViera <vieranoelia98@gmail.com>
2023-06-16 11:12:02 -0700

    Alphabet - ALPHABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet uppercase and lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+	char letra = 97;
+	char LETRA = 65; 
+
+	for (letra = 97; letra <= 122; letra++)
+		putchar(letra);
+	for (LETRA = 65; LETRA <= 90; LETRA++)
+		putchar(LETRA);
+	putchar('\n');
+	return (0);
+}
