487914473f6617e84e34e5b974da17268c11ecfa
MicaViera <vieranoelia98@gmail.com>
2023-06-18 22:10:05 -0700

    numbers base 10

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all the single numbers of base 10 starting from 0.
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar(num);
+	return (0);
+}
