15f202bb0214e0f092217dffec8d7f5e7b62613f
MicaViera <vieranoelia98@gmail.com>
2023-06-18 22:23:48 -0700

    Reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print all the lowercase alphabet in reverse.
+ * Return: 0
+ */
+int main(void)
+{
+	char rev;
+
+	for (rev = 12; rev <= 97; rev--)
+		putchar(rev);
+	putchar('\n');
+	return (0);
+}
