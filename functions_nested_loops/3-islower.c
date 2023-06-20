690b81da1e1fa24f87be5464166530108c40d05b
MicaViera <vieranoelia98@gmail.com>
2023-06-20 11:44:05 -0700

    Is Lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - Returns 1 if c is lowercase, else returns 0
+ * @c: received characters
+ *Return: 1 for lowercase. 0 for the rest.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
