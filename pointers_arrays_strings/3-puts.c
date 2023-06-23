5cfc660b6aca3b0479b2698bee57f11c3a36b9ea
MicaViera <vieranoelia98@gmail.com>
2023-06-23 07:20:04 -0700

    Puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  *_puts - prints a string.
  *@str: string.
@@ -7,6 +6,6 @@
 void _puts(char *str)
 {
 	while (*str != '\0')
-		putchar(*str), str++;
-	putchar('\n');
+		_putchar(*str), str++;
+	_putchar('\n');
 }
