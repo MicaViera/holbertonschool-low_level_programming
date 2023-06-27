adaefc2dbf08e80ab2a0108500130818918cbb4d
MicaViera <vieranoelia98@gmail.com>
2023-06-27 13:35:08 -0700

    Lower to Upper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *string_toupper - changes all lowercase letters of a string to uppercase.
+ * @a: value.
+ * Return: uppercase letters.
+ */
+char *string_toupper(char *a)
+{
+
+	int chan = 0;
+
+	while (a[chan++])
+	{
+		if (a[chan] >= 'a' && a[chan] <= 'z')
+			a[chan] -= - 32;
+
+	}
+	return (a);
+}
