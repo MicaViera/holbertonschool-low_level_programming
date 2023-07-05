72c7b2b87a477b708873f0e65f8676e31ce77296
MicaViera <vieranoelia98@gmail.com>
2023-07-05 13:42:26 -0700

    First Malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *create_array - Is a function that creates an array of chars.
+ * @size: the size.
+ * @c: specific char.
+ * Return: array of chars.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *a = NULL;
+
+	unsigned int r = 0;
+
+	if (size == 0)
+		return (NULL);
+	a = malloc(sizeof(char) * size);
+	if (!a)
+		return (NULL);
+
+	for (r = 0; r <= size; r++)
+		a[r] = c;
+	return (a);
+}
