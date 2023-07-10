8a4c885aaaae297b5ec9659ad1278918cdcb8699
MicaViera <vieranoelia98@gmail.com>
2023-07-10 12:02:53 -0700

    Define new structure

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+/**
+ * struct dog - Is a structure with variables.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The name of the owner.
+ */
 struct dog
 {
 	char *name;
