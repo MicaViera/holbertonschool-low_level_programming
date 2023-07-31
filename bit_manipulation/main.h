7c67c500a43e04aa228757f865caec106643a45c
MicaViera <vieranoelia98@gmail.com>
2023-07-30 19:28:41 -0700

    All the prototypes of the functions

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,7 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 /**
- * Prototypes.
+ * _putchar - writes the character c to stdout.
+ * @c: the charcter to print.
+ * Return: On success 1. On error -1.
  */
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
