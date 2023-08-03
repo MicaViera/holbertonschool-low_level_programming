361f9e60e8162ea16ad0c1b960949b3bf53b3c74
MicaViera <vieranoelia98@gmail.com>
2023-08-02 17:44:07 -0700

    Function that adds an element to the hash table.

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -32,8 +32,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	newnode->key = strdup(key);
 	newnode->value = strdup(value);
-	newnode->next = tmp;
-	tmp = newnode;
+	newnode->next = ht->array[index];
+	ht->array[index] = newnode;
 
 	return (1);
 }
