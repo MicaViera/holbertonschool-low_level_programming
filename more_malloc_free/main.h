fa8d12110a2a5ec85a797b5a29c72162afce4f6e
MicaViera <vieranoelia98@gmail.com>
2023-08-25 12:45:31 -0700

    Prototypes and libraries

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
 /**
  *Prototypes.
  */
@@ -5,3 +9,4 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+#endif
